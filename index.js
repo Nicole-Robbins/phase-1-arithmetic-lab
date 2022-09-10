0bfca8bf1f54157af89bc65ceb071912aa958cb6
Nicole Robbins <nicole.robbins@att.net>
2022-09-10 17:44:45 -0500

    commit tree 6f095db807447bdf18b81e7a599222078098f1be

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 2;
+let num2 = 31;
+let num3 = 9;
+let num4 = 5;
 
-// Write your code here
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random());
+let mod = num3 % num4;
+let max = Math.max(1, 3, 4, 20);
